144643949a8e76ae6fc966641df6fb4edc406f2b
Anita-Cahya-Pratiwi <129023785+Anita-Cahya-Pratiwi@users.noreply.github.com>
2025-01-04 21:37:32 +0700

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+//import RealTimeData from "./components/RealTimeData";  // Impor komponen RealTimeData
+import TextEditor from "./components/TextEditor";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aplikasi Kolaboratif React dengan Firebase Real-Time</h1>
+      {/*<RealTimeData />  */}
+      <TextEditor />
+    </div>
+  );
+}
+
+export default App;
