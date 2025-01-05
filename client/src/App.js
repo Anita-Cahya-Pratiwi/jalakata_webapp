3a8328fe57c8b8f0c93c8bb49bf04d4b0fc63768
Anita-Cahya-Pratiwi <129023785+Anita-Cahya-Pratiwi@users.noreply.github.com>
2025-01-05 20:24:14 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,40 @@
-import React from "react";
-//import RealTimeData from "./components/RealTimeData";  // Impor komponen RealTimeData
+import React, { useEffect, useState } from "react";
+import Auth from "./components/Auth";
 import TextEditor from "./components/TextEditor";
+import { auth } from './firebase-config';
+import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  // Fungsi logout dipindahkan ke App.js
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      setUser(null); // Mengubah status pengguna menjadi null setelah logout
+    } catch (error) {
+      console.error("Error logging out: ", error);
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      console.log("User status berubah: ", user); // Tambahkan log untuk melihat user
+      setUser(user);
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Aplikasi Kolaboratif React dengan Firebase Real-Time</h1>
-      {/*<RealTimeData />  */}
-      <TextEditor />
+      <h1>Dokumen Kolaboratif Jalakata</h1>
+      {!user && <Auth setUser={setUser} />}
+       {user && (
+        <button className="logout-button" onClick={handleLogout}>
+          Logout
+        </button>
+      )}
+      {user ? <TextEditor user={user} /> : <p>Silakan masuk untuk mengakses halaman teks editor.</p>}
     </div>
   );
 }
