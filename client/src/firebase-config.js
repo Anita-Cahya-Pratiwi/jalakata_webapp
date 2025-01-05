66bd7f04d5a3ce70f717188f8784611f9b7c42be
Anita-Cahya-Pratiwi <129023785+Anita-Cahya-Pratiwi@users.noreply.github.com>
2025-01-05 20:24:35 +0700

    Update firebase-config.js

--- client/src/firebase-config.js
+++ client/src/firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from "firebase/database";
+import { getAuth } from "firebase/auth"; 
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -13,5 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
+const auth = getAuth(app); 
 
-export { database, ref, set };
+export { database, ref, set, auth }; 
